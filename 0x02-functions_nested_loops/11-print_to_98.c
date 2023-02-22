a6699fb74e3c4e86e66eb2dd5c1fca34b2073a47
wntghartey <babynii1972@gmail.com>
2023-02-21 17:56:37 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,7 +14,7 @@ void print_to_98(int n)
 
 	if (n <= 98)
 	{
-for (n <= 98; n++);
+for (; n <= 98; n++);
 	{
 	if (n == 98)
 	{
@@ -30,7 +30,7 @@ for (n <= 98; n++);
 	}
 	else
 	{
-	for (n <= 98; n--;)
+	for (; n <= 98; n--;)
 	{
 	if (n == 98)
 	{
