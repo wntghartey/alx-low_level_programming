3357f34b89fc9ea99de240e59edb6aa8b995ef02
wntghartey <babynii1972@gmail.com>
2023-04-05 19:30:22 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,36 +11,40 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+listint_t *new_node, *prev_node, *curr_node;
 unsigned int i;
-listint_t *new;
-listint_t *temp = *head;
 
-new = malloc(sizeof(listint_t));
-if (!new || !head)
+if (head == NULL)
 return (NULL);
-
-new->n = n;
-new->next = NULL;
 
 if (idx == 0)
 {
-new->next = *head;
-*head = new;
-return (new);
-}
-
-for (i = 0; temp && i < idx; i++)
-{
-if (i == idx - 1)
-{
-new->next = temp->next;
-temp->next = new;
-return (new);
-}
-else
-temp = temp->next;
-}
-
+new_node = malloc(sizeof(listint_t));
+if (new_node == NULL)
 return (NULL);
+new_node->n = n;
+new_node->next = *head;
+*head = new_node;
+return (new_node);
 }
 
+prev_node = NULL;
+curr_node = *head;
+for (i = 0; i < idx && curr_node != NULL; i++)
+{
+prev_node = curr_node;
+curr_node = curr_node->next;
+}
+
+if (i != idx)
+return (NULL);
+
+new_node = malloc(sizeof(listint_t));
+if (new_node == NULL)
+return (NULL);
+new_node->n = n;
+new_node->next = curr_node;
+prev_node->next = new_node;
+
+return (new_node);
+}
