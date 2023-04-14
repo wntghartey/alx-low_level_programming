2920d0b3b8696d89233b1c16f00645b08872aac4
wntghartey <babynii1972@gmail.com>
2023-04-14 07:11:48 -0700

    1-create_file.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,10 +3,10 @@
 
 /**
  * read_textfile- Read text file print to STDOUT.
- * @filename: text file being read
- * @letters: number of letters to be read
- * Return: w- actual number of bytes read and printed
- *        0 when function fails or filename is NULL.
+ * @filename: text files  being read
+ * @letters: number of letters that are being read 
+ * Return: w- real number of bytes read and printed
+ *        0 when function stops  or filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,10 +6,10 @@ char *create_buffer(char *file);
 void close_file(int fd);
 
 /**
- * create_buffer - Allocates 1024 bytes for a buffer.
- * @file: The name of the file buffer is storing chars for.
+ * create_buffer - Assignes 1024 bytes for a buffer.
+ * @file: The name of the file buffer is keeping chars for.
  *
- * Return: A pointer to the newly-allocated buffer.
+ * Return: A pointer to the newly-assigned buffer.
  */
 char *create_buffer(char *file)
 {
@@ -28,8 +28,8 @@ return (buffer);
 }
 
 /**
- * close_file - Closes file descriptors.
- * @fd: The file descriptor to be closed.
+ * close_file -it  Closes file descriptors.
+ * @fd: The file descriptor is to be closed.
  */
 void close_file(int fd)
 {
@@ -45,15 +45,15 @@ exit(100);
 }
 
 /**
- * main - Copies the contents of a file to another file.
- * @argc: The number of arguments supplied to the program.
+ * main - Duplicates the contents of a file to another file.
+ * @argc: The number of arguments contibuted to the program.
  * @argv: An array of pointers to the arguments.
  *
  * Return: 0 on success.
  *
- * Description: If the argument count is incorrect - exit code 97.
- * If file_from does not exist or cannot be read - exit code 98.
- * If file_to cannot be created or written to - exit code 99.
+ * Description: If the argument count is  not correct - exit code 97.
+ * If file_from is not  existing or cannot be read - exit code 98.
+ * If file_to cannot be bulit or written to - exit code 99.
  * If file_to or file_from cannot be closed - exit code 100.
  */
 int main(int argc, char *argv[])
